d657f9feb6c1d98b2b048ff24e7a824f6c136f0c
fulvierella <99495379+fulvierella@users.noreply.github.com>
2022-03-30 16:57:52 +0200

    Injected data in the forecast slot

--- src/app.js
+++ src/app.js
@@ -21,6 +21,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+       <div class="weather-forecast-date"> ${day} </div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+          />
+        <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max"> 18° </span>
+         <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`; // CHIUDIAMO LA ROW
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showTemperature(response) {
   let tempElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -90,3 +120,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 search("New York");
+displayForecast();
